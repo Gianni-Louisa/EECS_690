bf915d59522b45c7c1e26b1a585b46b3fea92744
Manoj Turaga <manoj.turaga@ku.edu>
2025-04-15 22:02:29 -0500

    adding comments

--- src/modules/job.py
+++ src/modules/job.py
@@ -1,57 +1,108 @@
 from typing import Callable, Type
 
 class Job:
-    def __init__( self, job_id : int, job_priority : int, runtime : int, 
-                    release_time : int, error_func : Callable[ [ Type[ "Job" ] ], bool ],
+    def __init__( self, job_id : int, job_priority : int, runtime : float, 
+                    release_time : float, error_func : Callable[ [ Type[ "Job" ] ], bool ],
                     error_loc_func : Callable[ [ Type[ "Job" ] ], bool ], 
                     comparison_func : Callable[ [ Type[ "Job" ], Type[ "Job" ] ], int ] ):
+        
+        ################################################
         # The following are meta in formation about jobs 
+        ################################################
+        # This the unique identifier for a job
         self._job_id : int = job_id
+
+        # This is the priority of the job
         self._job_priority : int = job_priority
 
-        # The following are functions that define how jobs operate
+        ################################################
+        # The following are functions that define how 
+        # jobs operate
+        ################################################
+        # This function defines how jobs should be compared
+        # against eachother
         self._comparison_func = comparison_func
+
+        # This function defines how likely an error is to
+        # occur within a job
         self._error_func = error_func
+
+        # This function determines where an error is likely
+        # to occur
         self._error_loc_func = error_loc_func
         
-        # The following are information about the job
-        self._runtime : int = runtime
-        self._orig_runtime : int = runtime
-        self._release_time : int = release_time
-        self._first_schedule_time = -1
-        self._last_checkpoint_time : int = -1
-        self._active_running_time : int = 0
-        self._waiting_time : int = 0
+        ################################################
+        # The following is general information about the
+        # jobs current state and behavior
+        ################################################
+        # This is the remaining time a job needs to complete
+        self._runtime : float = runtime
+
+        # This is the original runtime of the job
+        self._orig_runtime : float = runtime
+
+        # This is when the job was released
+        self._release_time : float = release_time
+
+        # This is time that it was first scheduled
+        self._first_schedule_time : float = -1
+
+        # This is the last time a checkpoint was triggered
+        # on this job
+        self._last_checkpoint_time : float = -1
+
+        # This is the location of the last machine that this
+        # job was running on
+        self._last_run_machine : int = -1
+        
+        # This indicates whether this job is in error or not
+        self._in_error : bool = False
+        
+        ################################################
+        # These are statistics about the job
+        ################################################
+        # This is the time that the job was actively running
+        self._active_running_time : float = 0
+
+        # This is the job's completion time
         self._completion_time : float = 0
-        self._in_error = False
-        self._last_run_machine = -1
 
-    def get_first_schedule_time( self ):
+        # This is the time that the job spend waiting
+        self._waiting_time : float = 0
+
+    # This function returns the first scheduled time 
+    def get_first_schedule_time( self : type[ "Job" ] ):
         return self._first_schedule_time
-
+    
     def get_priority(self):
         return self._job_priority
-    
-    def get_release_time( self ):
-        return self._release_time
 
-    def set_first_schedule_time( self, time ):
+    # This function sets the first scheduled time
+    def set_first_schedule_time( self : type[ "Job" ], time : float ):
         self._first_schedule_time = time
         self._last_checkpoint_time = time
+    
+    # This function will get the release time
+    def get_release_time( self : type[ "Job" ] ):
+        return self._release_time
 
-    def get_id( self ):
+    # This function will get the jobs id
+    def get_id( self : type[ "Job" ] ):
         return self._job_id
     
-    def get_runtime( self ):
+    # This function will get the jobs current remaining runtime
+    def get_runtime( self : type[ "Job" ] ):
         return self._runtime
     
-    def restart_job( self ):
+    # This function will restart the job
+    def restart_job( self : type[ "Job" ] ):
         self._runtime = self._orig_runtime
         self._in_error = False
 
-    def get_orig_runtime( self ):
+    # This function will get the job's original runtime
+    def get_orig_runtime( self : type[ "Job" ] ):
         return self._orig_runtime
-
+    
     def get_waiting_time(self):
         return self._waiting_time
     
@@ -60,20 +111,36 @@ class Job:
         
     def get_completion_time( self ):
         return self._completion_time
-    
-    def progress( self, current_timestamp, inc = 1 ) -> float:
+
+    # This function will define the progression behavior
+    # of the job
+    def progress( self : type[ "Job" ], current_timestamp : float, inc : float = 1 ) -> float:
+        # First determine if the job is in error
         self._in_error = self._error_func( self, current_timestamp )
         
         if not self._in_error:
+            # If the job is not in error, determine
+            # maximum continuous unit of progressions we
+            # can take
             inc = min( self._runtime, inc )
+
+            # Reduce the runtime of the job
             self._runtime -= inc
+
+            # Increment the active runtime 
             self._active_running_time += inc
             return inc
         else:
+            # Determine where the location of the error could
+            # be if there is an error
             loc = self._error_loc_func( self, current_timestamp, inc )
+            
+            # Increment the active runtime but don't increment the
+            # job state because it is an invalid progression
             self._active_running_time += loc
             return loc
-        
+
+    # This function adds waiting time to the job    
     def add_waiting_time( self, waiting_time ):
         if waiting_time < 0:
             x = 1
@@ -81,33 +148,50 @@ class Job:
         #print('HERE')
         #print(waiting_time)
 
+    # This function determines if the job is complete
     def is_job_complete( self ):
         return self._runtime == 0
     
+    # This function determines a jobs active running time
     def get_job_active_running_time( self ):
         return self._active_running_time
     
+    # This function reverts a job to a previous state
     def revert_to_checkpoint( self, restart_point : int ):
+        # Removing the floating point error if it exists
         if restart_point < 0:
             restart_point = 0 
+        
+        # Reset the runtime
         self._runtime = restart_point
+
+        # Remove the error regardeless of whether it exists
+        # or not
         self._in_error = False
 
+    # This function determines if a job is in error
     def is_in_error( self ):
         return self._in_error
 
+    # This function updates the last running machine
     def set_last_run_machine( self, machine_id : int ):
         self._last_run_machine = machine_id
 
+    # This function returns the machine that the job last
+    # ran on 
     def get_last_run_machine( self ):
         return self._last_run_machine
     
+    # This function sets the last time this job was
+    # checkpointed
     def set_last_checkpoint_time( self, time ):
         self._last_checkpoint_time = time
 
+    # This function gets the last checkpointing time
     def get_last_checkpoint_time( self ):
         return self._last_checkpoint_time
     
+    # D
     def __eq__( self, other ):
         return self._comparison_func( self, other ) == 0
 
--- src/novelalgo.py
+++ src/novelalgo.py
@@ -17,12 +17,7 @@ def job_error_func( job : Job, current_timestamp ):
     p = 1 - ( math.e ) ** ( -LAMBDA * ( current_timestamp - job.get_last_checkpoint_time() ) )
     #print(p)
 
-    if p > ret:
-        #print('EEROR')
-        return True
-
-    else:
-        return False
+    return p > ret
     
 def job_comparison_func( job1 : Job, job2 : Job ):
     if job1._job_priority == job2._job_priority:
@@ -190,47 +185,56 @@ def curr_timestamp_func( scheduler : GlobalScheduler ):
         total_progress_map.sort( key=lambda x : x[ 1 ], reverse=True )
 
 
-scheduler = GlobalScheduler( 2, machine_progression_func, machine_checkpointing_func, new_job_func, reschedule_func, curr_timestamp_func )
+scheduler = GlobalScheduler( 3, machine_progression_func, machine_checkpointing_func, new_job_func, reschedule_func, curr_timestamp_func )
 
-# Currently I have it setup to randomly generate jobs, half at time 0 and the rest at random times
-def generate_random_jobs(num_jobs, max_priority, max_runtime, max_release_time): 
-    random_jobs = {} # dictionary of jobs at each timestamp
-    
-    jobs_at_time_0 = [] # jobs at time 0
-    for i in range(num_jobs // 2): # half of the jobs are released at time 0
-        job_id = i # job id
-        priority = random.randint(1, max_priority) # priority 
-        runtime = random.randint(1, max_runtime) # runtime
-        release_time = 0 # release time
-        
-        job = Job(job_id, priority, runtime, release_time, # job object
-                 job_error_func, # error function
-                 lambda x, y, z : z if z < 0.000001 else random.uniform( 0, z ), # error location function
-                 job_comparison_func) # job comparison function
-        jobs_at_time_0.append(job) # append job to jobs at time 0
-    
-    jobs_at_random_times = {} # dictionary of jobs at random times
-    for i in range(num_jobs // 2, num_jobs): # remaining jobs
-        job_id = i # job id
-        priority = random.randint(1, max_priority) # priority
-        runtime = random.randint(1, max_runtime) # runtime
-        release_time = random.randint(1, max_release_time) # release time
-        
-        job = Job(job_id, priority, runtime, release_time, # job object
-                 job_error_func, # error function
-                 lambda x, y, z : z if z < 0.000001 else random.uniform( 0, z ), # error location function
-                 job_comparison_func) # job comparison function
-        
-        if release_time not in jobs_at_random_times: 
-            jobs_at_random_times[release_time] = [] # if the release time is not in the dictionary, create a new list
-        jobs_at_random_times[release_time].append(job) # append the job to the list
-    
-    random_jobs[0] = jobs_at_time_0 # add the jobs at time 0 to the dictionary
-    for release_time, jobs in jobs_at_random_times.items():
-        random_jobs[release_time] = jobs # add the jobs at random times to the dictionary
-    
-    return random_jobs
+jobs = { 0 : [
+        Job(0, 1, 2, 0, job_error_func, lambda x, y, z : z, job_comparison_func),
+        Job(1, 2, 2, 0, job_error_func, lambda x, y, z : z, job_comparison_func),
+        Job(2, 3, 2, 0, job_error_func, lambda x, y, z : z, job_comparison_func),
+        Job(3, 4, 2, 0, job_error_func, lambda x, y, z : z, job_comparison_func),
+        Job(4, 5, 2, 0, job_error_func, lambda x, y, z : z, job_comparison_func),
+        Job(5, 6, 2, 0, job_error_func, lambda x, y, z : z, job_comparison_func),
+        Job(6, 7, 2, 0, job_error_func, lambda x, y, z : z, job_comparison_func),
+        Job(7, 8, 2, 0, job_error_func, lambda x, y, z : z, job_comparison_func),
+    ] }
 
-#jobs = generate_random_jobs(num_jobs=10000, max_priority=5, max_runtime=5, max_release_time=1)
+# # Currently I have it setup to randomly generate jobs, half at time 0 and the rest at random times
+# def generate_random_jobs(num_jobs, max_priority, max_runtime, max_release_time): 
+#     random_jobs = {} # dictionary of jobs at each timestamp
+    
+#     jobs_at_time_0 = [] # jobs at time 0
+#     for i in range(num_jobs // 2): # half of the jobs are released at time 0
+#         job_id = i # job id
+#         priority = random.randint(1, max_priority) # priority 
+#         runtime = random.randint(1, max_runtime) # runtime
+#         release_time = 0 # release time
+        
+#         job = Job(job_id, priority, runtime, release_time, # job object
+#                  job_error_func, # error function
+#                  lambda x, y, z : z if z < 0.000001 else random.uniform( 0, z ), # error location function
+#                  job_comparison_func) # job comparison function
+#         jobs_at_time_0.append(job) # append job to jobs at time 0
+    
+#     jobs_at_random_times = {} # dictionary of jobs at random times
+#     for i in range(num_jobs // 2, num_jobs): # remaining jobs
+#         job_id = i # job id
+#         priority = random.randint(1, max_priority) # priority
+#         runtime = random.randint(1, max_runtime) # runtime
+#         release_time = random.randint(1, max_release_time) # release time
+        
+#         job = Job(job_id, priority, runtime, release_time, # job object
+#                  job_error_func, # error function
+#                  lambda x, y, z : z if z < 0.000001 else random.uniform( 0, z ), # error location function
+#                  job_comparison_func) # job comparison function
+        
+#         if release_time not in jobs_at_random_times: 
+#             jobs_at_random_times[release_time] = [] # if the release time is not in the dictionary, create a new list
+#         jobs_at_random_times[release_time].append(job) # append the job to the list
+    
+#     random_jobs[0] = jobs_at_time_0 # add the jobs at time 0 to the dictionary
+#     for release_time, jobs in jobs_at_random_times.items():
+#         random_jobs[release_time] = jobs # add the jobs at random times to the dictionary
+    
+#     return random_jobs
 
-#scheduler.run_schedule( jobs )
+# jobs = generate_random_jobs(num_jobs=10, max_priority=5, max_runtime=5, max_release_time=1)
